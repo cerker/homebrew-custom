13cef01c23f9ee0e12fc6075202fbb558a907b49
Carsten Erker <carsten.erker@akquinet.de>
2013-02-08 18:19:53 +0100

    correction userpath

--- dblatex.rb
+++ dblatex.rb
@@ -4,7 +4,8 @@ class Dblatex < Formula
   url 'http://downloads.sourceforge.net/project/dblatex/dblatex/dblatex-0.3/dblatex-0.3.tar.bz2'
   homepage 'http://dblatex.sourceforge.net'
   md5 '7de6bf72b8b2934169ce0ec911e966ed'
-  env :userpath
+
+  env :userpaths
 
   def install
     system "python", "setup.py", "install", "--prefix=#{prefix}", "--install-scripts=#{bin}"
